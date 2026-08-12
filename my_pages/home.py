6433937f4dc9f1ac03d10b363f8d6c9efb6e20dd
rakeshg9226-dads <rakesh.g.9226@gmail.com>
2026-08-12 13:02:37 +0100

    Fix typo in subheader function name

--- my_pages/home.py
+++ my_pages/home.py
@@ -4,4 +4,4 @@ import streamlit as st
 def show():
     st.title("🏠 Home")
     st.header("Welcome to my Streamlit application.")
-    st.subheade("This is my first app in streamlit using python")
+    st.subheader("This is my first app in streamlit using python")
